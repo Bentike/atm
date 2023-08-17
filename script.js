596bf91f8e62f16ce6d248d262ddeb0ac33068b0
bentike <ajadiyusuf73@gmail.com>
2023-08-17 14:16:08 +0100

    removed quickteller from options

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ let cardPin = prompt("Enter Card Pin");
 if (cardPin !== defaultPin) {
   alert("incorrect pin");
 }
-let menu = prompt("Select An Option \n 1. Qiuckteller \n 2. Transfer \n 3. Airtime \n 4. Withdrawal \n 5. Check Balance");
-if(menu === "4"){
+let menu = prompt("Select An Option \n 1. Transfer \n 2. Airtime \n 3. Withdrawal \n 4. Check Balance");
+
+
+// WITHDRAWAL;
+if(menu === "3"){
     let accountType = prompt("1. Savings \n 2. Current");
     let amountToWithdraw = prompt("1000 \n 5000 \n 10000 \n Others");
     if(amountToWithdraw === "1000" || amountToWithdraw === "5000" || amountToWithdraw === "10000"){
